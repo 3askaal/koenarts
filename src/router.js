20a7a7dd7ab1483ac91b52c377365dbf65d80df7
Bas Kaal <3askaal@gmail.com>
2020-04-30 23:49:04 +0200

    progress

--- src/router.js
+++ src/router.js
@@ -15,7 +15,7 @@ import {
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     { path: '/', name: 'home', component: HomePage },
